09a92c86bb2043bf5c205e1018cb390badf724a7
John Doe <johndoe@example.com>
2024-09-13 13:36:08 +0100

    clean code

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,35 +19,3 @@ with tab1:
 tab2.write("this is tab 2")
 tab3.write("this is tab 3")
 tab4.write("this is tab 4")
-
-
-'''
-# Simuler des données
-np.random.seed(0)
-dates = pd.date_range('2015-01-01', periods=2000, freq='D')
-data = {
-    'Date': dates,
-    'Accidents': np.random.poisson(2, size=len(dates)),
-    'Weather_Condition': np.random.choice(['Clear', 'Rainy', 'Snowy', 'Foggy'], size=len(dates)),
-    'Weekend': (dates.weekday >= 5).astype(int)
-}
-df = pd.DataFrame(data)
-
-# Convertir 'Date' en index datetime
-df['Date'] = pd.to_datetime(df['Date'])
-df.set_index('Date', inplace=True)
-
-# Ajouter des colonnes pour l'année, le mois, et le jour de la semaine
-df['Year'] = df.index.year
-df['Month'] = df.index.month
-df['Weekday'] = df.index.weekday
-
-print(df.head())
-
-x = df['Year'].unique()
-
-with st.sidebar:
-    st.radio("Select one:", [1, 2])
-    # Sélectionner l'année
-    st.selectbox('Sélectionner l\'année:', x)
-'''
